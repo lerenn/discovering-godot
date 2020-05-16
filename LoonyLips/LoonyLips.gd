805ab340c61281c2421533c9876e6ffed60c0a3b
Louis FRADIN <louis.fradin@gmail.com>
2020-05-16 15:52:25 +0200

    LoonyLips: Add the end of the game

--- LoonyLips/LoonyLips.gd
+++ LoonyLips/LoonyLips.gd
@@ -1,24 +1,42 @@
 extends Control
 
 var player_words = []
-var story = """Once upon a time someone called %s ate a %s flavoured sandwich
-	which made him feel all %s inside. It was a %s day."""
-var prompts = [ "a name", "a noun", "adverb", "adjective" ]
+var current_story = {}
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
 onready var ButtonLabel = $VBoxContainer/HBoxContainer/Label
 
 func _ready():
+	set_current_story()
 	DisplayText.text = """Welcome to Loony Lips! We're going to tell a story and
 		have a wonderful time!"""
 	check_player_words_length()
+
+func set_current_story():
+	randomize()
+	
+#	var stories = get_from_json("StoryBook.json")
+#	current_story = stories[randi() % stories.size()]
+	
+	var stories = $StoryBook.get_child_count()
+	var selected_story = randi() % stories
+	current_story = $StoryBook.get_child(selected_story)
+
+func get_from_json(filename):
+	var file = File.new()
+	file.open(filename, File.READ)
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	return data
 
 func _on_PlayerText_text_entered(_new_text):
 	add_to_player_words()
 
 func _on_TextureButton_pressed():
 	if is_story_done():
+		# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 	else:
 		add_to_player_words()
@@ -30,7 +48,7 @@ func add_to_player_words():
 	check_player_words_length()
 
 func is_story_done():
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 
 func check_player_words_length():
 	if is_story_done():
@@ -39,11 +57,11 @@ func check_player_words_length():
 		prompt_player()
 		
 func tell_story():
-	DisplayText.text = story % player_words
+	DisplayText.text = current_story.story % player_words
 
 func prompt_player():
 	var position = player_words.size()
-	DisplayText.text += " May I have %s ?" % prompts[position]
+	DisplayText.text += " May I have %s ?" % current_story.prompts[position]
 
 func end_game():
 	PlayerText.queue_free()
